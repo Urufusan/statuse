04204244acd040ac32a07debf442360e0d97ced9
Lavender Perry <endie2@protonmail.com>
2022-05-28 18:05:35 -0700

    Styling.

--- src/content.rb
+++ src/content.rb
@@ -4,9 +4,8 @@ def get_plaintext_stats
   "OS: #{OS::NAME}\n" +
     "Device: #{CPU::INFO["model"]}\n" +
     "Processors: #{CPU::INFO[:processors]}\n" +
-    "BogoMIPS: #{CPU::INFO["bogomips"]}\n" +
-    "CPU usage: #{CPU.usage}%\n" +
     "Processes: #{OS.processes}\n" +
+    "CPU usage: #{CPU.usage}%\n" +
     "System time: #{Time.now}\n"
 end
 
