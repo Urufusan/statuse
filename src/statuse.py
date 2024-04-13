f697cb9a854d78da9b6b738e4d2e177ad27ccf6b
Urufusan <63711492+Urufusan@users.noreply.github.com>
2024-04-13 11:41:19 +0200

    Nonstandard port definition

--- src/statuse.py
+++ src/statuse.py
@@ -27,4 +27,4 @@ def index():
         return render_template("index.html", stats_obj=Stats)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="8008", debug=False)
+    app.run(host="0.0.0.0", port=8008, debug=False)
